0000844686c9839a2a0759763cfa0b3e60e70c71
ZAPHODh <102484511+ZAPHODh@users.noreply.github.com>
2022-11-29 00:47:09 -0300

    back to usual last parameters

--- next.config.js
+++ next.config.js
@@ -9,9 +9,14 @@ const nextConfig = {
     styledComponents: true,
   },
   trailingSlash: true,
-  env: {
-    LOGIN_URL: process.env.NEXT_PLUBIC_API_LOGIN,
-    REGISTER_URL: process.env.NEXT_PLUBIC_API_REGISTER,
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination:
+          'https://ec2-54-94-101-205.sa-east-1.compute.amazonaws.com:3000',
+      },
+    ];
   },
 };
 
