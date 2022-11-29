7b44885b44412c819a775c992cb0500606733308
ZAPHODh <102484511+ZAPHODh@users.noreply.github.com>
2022-11-29 00:34:42 -0300

    removing all different

--- next.config.js
+++ next.config.js
@@ -9,15 +9,6 @@ const nextConfig = {
     styledComponents: true,
   },
   trailingSlash: true,
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination:
-          'https://ec2-54-94-101-205.sa-east-1.compute.amazonaws.com:3000',
-      },
-    ];
-  },
   env: {
     LOGIN_URL: process.env.NEXT_PLUBIC_API_LOGIN,
     REGISTER_URL: process.env.NEXT_PLUBIC_API_REGISTER,
