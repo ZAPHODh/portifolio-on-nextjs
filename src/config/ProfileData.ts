cc1e0ae315d24db0c7732007e3fee438f40b74c3
ZAPHODh <102484511+ZAPHODh@users.noreply.github.com>
2022-12-02 19:42:21 -0300

    chaging profile pic

--- src/config/ProfileData.ts
+++ src/config/ProfileData.ts
@@ -2,8 +2,7 @@ import { ProfileProps } from 'components/Profile';
 
 export const profileData: ProfileProps = {
   name: 'Luis Paulo Martins',
-  photo:
-    'https://pps.whatsapp.net/v/t61.24694-24/311852977_3115775005381306_4184797310031893050_n.jpg?ccb=11-4&oh=01_AdSqfsDGrEPyLIUWSU5qMA_PK-NWpLDevfY7wZ51S3Q8Ng&oe=638888EC',
+  photo: 'https://ibb.co/B3mqv5h',
   description:
     'Olá, Me chamo Luis. Gosto de tecnologia e computação desde criança. Programo em javascript há dois anos. Abaixo, mostratei um pouco de meus conhecimentos.',
   socialMedia: [
