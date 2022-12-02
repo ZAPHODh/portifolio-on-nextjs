e1134deed345f18bd41c08079a9f39013ae01a89
ZAPHODh <102484511+ZAPHODh@users.noreply.github.com>
2022-12-01 21:53:02 -0300

    tryng to solve the problem

--- next.config.js
+++ next.config.js
@@ -9,15 +9,6 @@ const nextConfig = {
     styledComponents: true,
   },
   trailingSlash: true,
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination:
-          'https://a876-2804-d41-ef30-c600-a1c7-b72c-7ecd-447.sa.ngrok.io/user/:path',
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
