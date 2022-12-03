a24c89ddb5730f8402cd1ddcd1ba61930c428b34
ZAPHODh <102484511+ZAPHODh@users.noreply.github.com>
2022-12-03 16:24:13 -0300

    changing color 2

--- src/components/Footer/styles.tsx
+++ src/components/Footer/styles.tsx
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components';
 export const Wrapper = styled.div`
   ${({ theme }) => css`
   width: 100vw;
-  background-color: #333333;
+  background-color: #0001c;
   color:${theme.colors.mediumGray};
   height: 150px;
   display: flex;
