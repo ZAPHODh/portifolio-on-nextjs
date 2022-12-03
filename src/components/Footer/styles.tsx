bdf914e26191891843401b0f91f90e02abd61a53
ZAPHODh <102484511+ZAPHODh@users.noreply.github.com>
2022-12-03 16:27:20 -0300

    hex error

--- src/components/Footer/styles.tsx
+++ src/components/Footer/styles.tsx
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components';
 export const Wrapper = styled.div`
   ${({ theme }) => css`
   width: 100vw;
-  background-color: #0001c;
+  background-color: #00001c;
   color:${theme.colors.mediumGray};
   height: 150px;
   display: flex;
